23b41c8847e660adeaea584bb0b6e41d506bdca7
Chandresh Tank <chandresh.tank@chamberlain.com>
2024-05-22 17:06:06 -0500

    Added pass by reference and value object

--- cppScheduler.cpp
+++ cppScheduler.cpp
@@ -4,20 +4,40 @@ using namespace std;
 namespace chandresh {
     int valueOfA = 10;
 }
-
+enum TASKPRIORITY {
+    LOW = 0,
+    MEDIUM = 1,
+    HIGH = 2
+} ;
 
 class task {
     public:
         int taskId;
+        TASKPRIORITY taskPriority;
 
-        task(int id): taskId(id)
+        task(int id): taskId(id), taskPriority(MEDIUM)
         {
             cout << "Task with TaskID " << id << "created." << endl;
         }
 
-        void printTaskID()
+        task(int id, TASKPRIORITY prio): taskId(id), taskPriority(prio)
         {
-            cout << "TaskID is " << taskId << endl;
+            cout << "Task with TaskID " << id << "created with Priority " << prio << endl;
+        }
+
+        void printTaskDetails(task tsk)
+        {
+            cout << "TaskID is " << taskId << "and Priority " << tsk.taskPriority << endl;
+        }
+
+//        Doing it like this will call the copy constructor while and copy the taskObj from caller function and past in tsk object so base taskObj function will never get the changes reflected.  
+//        void modifyTaskAttr(task tsk, TASKPRIORITY newPrio)
+
+//        Doing it like this, The object is passed by reference so the changes made in this function in the tsk object will be reflected in called object i.e taskObj
+        void modifyTaskAttr(task &tsk, TASKPRIORITY newPrio)
+        {
+            tsk.taskPriority = newPrio;
+            cout << "Modify :TaskID " << tsk.taskId << "modified and Priority " << tsk.taskPriority << endl;
         }
 };
 
@@ -42,5 +62,13 @@ int main()
     taskScheduler schedulerObject;
 
     schedulerObject.printNumOfTasks();
+
+    taskObj.printTaskDetails(taskObj);
+
+    taskObj.modifyTaskAttr(taskObj, HIGH);
+
+    taskObj.printTaskDetails(taskObj);
+
+    cout << sizeof(taskObj) << " is the size of the object" ;
 
 }
